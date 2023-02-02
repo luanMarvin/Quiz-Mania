ccd5a0efc270ca275fe7ef432ea7c9eb4d194ba0
luanMarvin <luanmarvin@gmail.com>
2023-02-01 21:09:10 -0300

    Mudanças iniciais estrutura web

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const routes = require('./routes')
+const express = require('express');
+const routes = require('./routes');
+const path = require('path')
 
 const app = express();
 app.set('view engine' , 'ejs');
-//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 //app.use(express.urlencoded({ extended: true }));
 app.use('/', routes)
 
